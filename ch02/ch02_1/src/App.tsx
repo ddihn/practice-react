3409ccbed1feed0e2ae107f4848eaa297d1d17da
Yoon <sooin0105@gmail.com>
2025-06-09 21:06:06 +0900

    chore: save progress up to event listeners

---
+++ ch02/ch02_1/src/App.tsx
@@ -0,0 +1,9 @@
+import ArrowComponent from './ArrowComponent'
+
+export default function App() {
+  return (
+    <ul>
+      <ArrowComponent href="https://google.com" text="go to google"></ArrowComponent>
+    </ul>
+  )
+}
